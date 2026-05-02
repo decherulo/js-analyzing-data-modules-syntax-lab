ded486912b6952be1ba5e84c5ce80b9190a556e8
Deborah Kimtai <decherulo@gmail.com>
2026-05-02 23:57:30 +0300

    complete combineUsers function

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+const datejs = require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
 
+  for (const array of args) {
+    combinedObject.users = [...combinedObject.users, ...array];
+  }
+
+  combinedObject.merge_date = new Date().toString('M/d/yyyy');
+
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
